76608f7cdab6b6726856c0ae1176297a01fdc6cf
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-06-13 17:34:50 -0300

    change: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
 import { Header } from "../components/Header";
+import { About } from "../components/About";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <section className="h-screen bg-gradient-to-b from-gray-200 via-white to-gray-200 ">
+        <About />
+      </section>
     </>
 
   )
