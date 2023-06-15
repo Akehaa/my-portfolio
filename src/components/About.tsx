8de16ef5daeca24397139e1a91d77a2185e00d04
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-06-15 17:49:01 -0300

    add:Projects component with Exclusive Store project

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,10 @@ export function About() {
     <div className="mx-8 pt-40 md:mx-auto w-3/4 2xl:w-2/4 lg:pt-0 ">
       <span className="text-xs">A LITTLE</span>
       <h2 className="text-xl mb-8">ABOUT ME</h2>
-      <p className="shadow-inner shadow-black bg-gradient-to-l from-gray-200 via-white to-gray-200 rounded-2xl p-8 text-sm lg:text-lg lg:p-12">
+      <p className="shadow-inner shadow-gray-300 bg-gradient-to-l from-gray-200 via-white to-gray-200 rounded-2xl p-8 text-sm lg:text-lg lg:p-12">
         A 24-year-old Front-end React Developer with over a year of coding experience. I gained my coding skills through the <Link href="https://www.rocketseat.com.br/ignite?utm_source=site_rocketseat&utm_medium=tabs_programas&utm_campaign=redirecionameto&utm_term=organic&utm_content=button" className="text-[#8257E5] hover:font-bold" target="_blank">Rocketseat ReactJS Specialization Bootcamp</Link>. You can find the projects on my <Link href="https://github.com/Akehaa?tab=repositories" className="text-redishpinksh hover:font-bold" target="_blank">GitHub</Link>.<br />
         I'm used to code using NextJS, TypeScript, TailwindCSS with PostCSS and to use Figma. I'm constantly exploring new technologies and i really enjoy staying up to date with the latest trends in the market.
       </p>
     </div>
-
   )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import exclusiveStoreHome from "@/public/exclusive-store-home.jpg";
+import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
+import { BiLinkExternal } from 'react-icons/bi'
+
 export function Projects() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="mx-8 pt-40 md:mx-auto w-3/4 2xl:w-2/4">
+        <span className="text-xs">SOME OF</span>
+        <h2 className="text-xl mb-8">MY PROJECTS</h2>
+      </div>
+      <div className="pb-20">
+        <div className="shadow-inner shadow-gray-300 bg-gradient-to-l from-gray-200 via-white to-gray-200 rounded-2xl mx-8 w-3/4 md:mx-auto lg:text-lg lg:p-12 2xl:w-2/4">
+          <div className="flex flex-col lg:flex-row lg:gap-8">
+            <Link href="https://github.com/Akehaa/exclusive-store" target="_blank" aria-label="GitHub Repo">
+              <Image src={exclusiveStoreHome} width={900} height={0} alt="exclusive store home page" className="h-auto rounded" />
+            </Link>
+            <div className="p-6 text-sm lg:p-0 lg:flex lg:flex-col lg:justify-between">
+              <div>
+                <h3 className="text-lg text-center mb-4 font-bold lg:text-left">Exclusive Store</h3>
+                <p className="mb-4">Exclusive is a really complete e-commerce. It has multiple pages, product page, sliders, wishlist, cart, checkout, etc.</p>
+              </div>
+              <div className="flex flex-col mb-4">
+                <span className="font-bold mb-4 text-center lg:text-left">It uses:</span>
+                <p>NextJS 13, TypeScript, TailwindCSS with PostCSS, Stripe API, React-Slick, Material UI and Radix-UI.</p>
+              </div>
+              <div className="flex items-center justify-center gap-12 lg:justify-normal">
+                <Link href="https://github.com/Akehaa/exclusive-store" target="_blank" aria-label="GitHub Repo" className="flex items-center hover:text-redishpinksh duration-200">
+                  <AiFillGithub size='auto' className="w-10 p-1" />
+                  <span className="font-bold">Repo</span>
+                </Link>
+                <Link href="https://akehaa-exclusive-store.vercel.app/" target="_blank" aria-label="Live" className="flex items-center hover:text-redishpinksh duration-200">
+                  <BiLinkExternal size='auto' className="w-9 p-1" />
+                  <span className="font-bold">Live</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
