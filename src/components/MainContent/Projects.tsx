27d67c8e7f33c65652912e48042c69f29be0aebe
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-06-16 11:11:24 -0300

    change responsiveness

--- src/components/MainContent/Projects.tsx
+++ src/components/MainContent/Projects.tsx
@@ -4,7 +4,7 @@ import { ExclusiveStore } from "./Projects/ExclusiveStore";
 export function Projects() {
   return (
     <>
-      <div className="mx-8 pt-40 md:mx-auto w-3/4 2xl:w-2/3 3xl:w-2/4">
+      <div className="mx-8 pt-40 md:mx-auto w-3/4 xl:w-3/4 3xl:w-2/4">
         <span className="text-xs">SOME OF</span>
         <h2 className="text-xl mb-8">MY PROJECTS</h2>
       </div>
