a972d73a2e658aa7c7ba5879a2985470c3462cfa
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-11-18 13:55:31 -0300

    update skills

--- src/components/Header/Skills.tsx
+++ src/components/Header/Skills.tsx
@@ -59,23 +59,6 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default' aria-label='Next.js'>
-            <SiNextdotjs size="auto" className="w-9 hover:-translate-y-2 duration-200 lg:w-11" />
-          </button>
-        </Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content
-            className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-black text-white px-[14px] py-[8px] text-[14px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] -translate-y-1"
-            sideOffset={5}
-          >
-            Next.js
-            <Tooltip.Arrow className="fill-black" />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-
-      <Tooltip.Root>
-        <Tooltip.Trigger asChild>
           <button className='cursor-default' aria-label='TypeScript'>
             <SiTypescript size="auto" className="text-[#2F72BC] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
@@ -103,6 +86,23 @@ export function Skills() {
             sideOffset={5}
           >
             React
+            <Tooltip.Arrow className="fill-black" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button className='cursor-default' aria-label='Next.js'>
+            <SiNextdotjs size="auto" className="w-9 hover:-translate-y-2 duration-200 lg:w-11" />
+          </button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-black text-white px-[14px] py-[8px] text-[14px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] -translate-y-1"
+            sideOffset={5}
+          >
+            Next.js
             <Tooltip.Arrow className="fill-black" />
           </Tooltip.Content>
         </Tooltip.Portal>
