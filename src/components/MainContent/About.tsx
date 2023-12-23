0206b970605d32ccc792a366f490aa301333c35d
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-12-23 15:14:59 -0300

    update About

--- src/components/MainContent/About.tsx
+++ src/components/MainContent/About.tsx
@@ -6,7 +6,7 @@ export function About() {
       <span className="text-xs">A LITTLE</span>
       <h2 className="text-xl mb-8">ABOUT ME</h2>
       <p className="shadow-inner shadow-gray-300 bg-gradient-to-l from-gray-200 via-white to-gray-200 rounded-2xl p-8 text-sm lg:text-lg lg:p-12">
-        A 24-years-old Developer with over a year of coding experience. I gained my coding skills through the <Link href="https://www.rocketseat.com.br/ignite?utm_source=site_rocketseat&utm_medium=tabs_programas&utm_campaign=redirecionameto&utm_term=organic&utm_content=button" className="text-[#8257E5]" target="_blank">Rocketseat Bootcamp</Link>. You can find the projects on my <Link href="https://github.com/deneruaraujo?tab=GitHubsitories" className="text-redishpinksh" target="_blank">GitHub</Link>.
+        A 24-years-old Full Stack Developer with over a year of coding experience. You can find the projects on my <Link href="https://github.com/deneruaraujo?tab=GitHubsitories" className="text-redishpinksh" target="_blank">GitHub</Link>.
         <br />
         <br />   
         My <span className="text-redishpinksh font-bold">Front-end</span>  experiences lies in <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">React</span>, <span className="font-bold">Next.js</span>, <span className="font-bold">TailwindCSS</span>, UIs like <span className="font-bold">MUI</span> and <span className="font-bold">Radix-UI</span>, and <span className="font-bold">Figma</span>.<br />
