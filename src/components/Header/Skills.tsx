6ae41498295482dca5548d0a1eba3be5a99963db
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2024-03-22 11:51:24 -0300

    Update: Descriptions, skills and Resume

--- src/components/Header/Skills.tsx
+++ src/components/Header/Skills.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as Tooltip from '@radix-ui/react-tooltip';
-import { SiCss3, SiHtml5, SiJavascript, SiNextdotjs, SiReact, SiTailwindcss, SiTypescript, SiNodedotjs, SiNestjs, SiVitest, SiJest, SiPostgresql, SiDocker, SiPrisma } from 'react-icons/si';
+import { SiCss3, SiHtml5, SiJavascript, SiNextdotjs, SiReact, SiTailwindcss, SiTypescript, SiNodedotjs, SiNestjs, SiVitest, SiJest, SiPostgresql, SiDocker, SiPrisma, SiRedux } from 'react-icons/si';
 
 export function Skills() {
   return (
@@ -110,7 +110,24 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.07s_ease-in-out_infinite] hover:animate-pulse' aria-label='Tailwind'>
+          <button className='cursor-default animate-[bounce_1.07s_ease-in-out_infinite] hover:animate-pulse' aria-label='Redux'>
+            <SiRedux size="auto" className="w-9 hover:-translate-y-2 duration-200 lg:w-11" />
+          </button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-black text-white px-[14px] py-[8px] text-[14px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] -translate-y-1"
+            sideOffset={5}
+          >
+            Redux
+            <Tooltip.Arrow className="fill-black" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button className='cursor-default animate-[bounce_1.08s_ease-in-out_infinite] hover:animate-pulse' aria-label='Tailwind'>
             <SiTailwindcss size="auto" className="text-[#35B3EB] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -127,7 +144,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.08s_ease-in-out_infinite] hover:animate-pulse' aria-label='Node.js'>
+          <button className='cursor-default animate-[bounce_1.09s_ease-in-out_infinite] hover:animate-pulse' aria-label='Node.js'>
             <SiNodedotjs size="auto" className="text-[#80BD41] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -144,7 +161,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.09s_ease-in-out_infinite] hover:animate-pulse' aria-label='NestJS'>
+          <button className='cursor-default animate-[bounce_1.10s_ease-in-out_infinite] hover:animate-pulse' aria-label='NestJS'>
             <SiNestjs size="auto" className="text-[#E0234E] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -161,7 +178,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.10s_ease-in-out_infinite] hover:animate-pulse' aria-label='Vitest'>
+          <button className='cursor-default animate-[bounce_1.11s_ease-in-out_infinite] hover:animate-pulse' aria-label='Vitest'>
             <SiVitest size="auto" className="text-[#2D3748] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -178,7 +195,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.11s_ease-in-out_infinite] hover:animate-pulse' aria-label='Jest'>
+          <button className='cursor-default animate-[bounce_1.12s_ease-in-out_infinite] hover:animate-pulse' aria-label='Jest'>
             <SiJest size="auto" className="text-[#2D3748] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -195,7 +212,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.12s_ease-in-out_infinite] hover:animate-pulse' aria-label='PostgreSQL'>
+          <button className='cursor-default animate-[bounce_1.13s_ease-in-out_infinite] hover:animate-pulse' aria-label='PostgreSQL'>
             <SiPostgresql size="auto" className="text-[#336791] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -212,7 +229,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.13s_ease-in-out_infinite] hover:animate-pulse' aria-label='Docker'>
+          <button className='cursor-default animate-[bounce_1.14s_ease-in-out_infinite] hover:animate-pulse' aria-label='Docker'>
             <SiDocker size="auto" className="text-[#1D63ED] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
@@ -229,7 +246,7 @@ export function Skills() {
 
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          <button className='cursor-default animate-[bounce_1.14s_ease-in-out_infinite] hover:animate-pulse' aria-label='Prisma'>
+          <button className='cursor-default animate-[bounce_1.15s_ease-in-out_infinite] hover:animate-pulse' aria-label='Prisma'>
             <SiPrisma size="auto" className="text-[#2D3748] w-9 hover:-translate-y-2 duration-200 lg:w-11" />
           </button>
         </Tooltip.Trigger>
--- src/components/MainContent/About.tsx
+++ src/components/MainContent/About.tsx
@@ -6,15 +6,15 @@ export function About() {
       <span className="text-xs">A LITTLE</span>
       <h2 className="text-xl mb-8">ABOUT ME</h2>
       <p className="shadow-inner shadow-gray-300 bg-gradient-to-l from-gray-200 via-white to-gray-200 rounded-2xl p-8 text-sm lg:text-lg lg:p-12">
-        A 24-years-old Full Stack Developer with over a year of coding experience. You can find the projects on my <Link href="https://github.com/deneruaraujo?tab=GitHubsitories" className="text-redishpinksh" target="_blank">GitHub</Link>.
+        I'm a 24-year-old Full Stack Developer with Advanced English(C1). You can find my projects on <Link href="https://github.com/deneruaraujo?tab=GitHubsitories" className="text-redishpinksh" target="_blank">GitHub</Link>.
         <br />
-        <br />   
-        My <span className="text-redishpinksh font-bold">Front-end</span>  experiences lies in <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">React</span>, <span className="font-bold">Next.js</span>, <span className="font-bold">TailwindCSS</span>, UIs like <span className="font-bold">MUI</span> and <span className="font-bold">Radix-UI</span>, and <span className="font-bold">Figma</span>.<br />
-        <br />   
-        My <span className="text-redishpinksh font-bold">Back-end</span> experiences lies in <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">Node.js</span>, <span className="font-bold">NestJS</span>, <span className="font-bold">Vitest</span>, <span className="font-bold">Jest</span>, <span className="font-bold">PostgreeSQL</span>, <span className="font-bold">Docker</span>, <span className="font-bold">Prisma</span> and <span className="font-bold">Cloudflare R2</span>.
-        <br />   
         <br />
-        I'm constantly exploring new technologies and i really enjoy staying up to date with the latest trends in the market.
+        My <span className="text-redishpinksh font-bold">Front-end</span> experience lies in <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">React</span>, <span className="font-bold">Next.js</span>, <span className="font-bold">Redux</span>, <span className="font-bold">Context API</span>, <span className="font-bold">TailwindCSS</span>, UIs like <span className="font-bold">MUI</span>, <span className="font-bold">Radix-UI</span> and <span className="font-bold">shadcn/ui</span>. Also <span className="font-bold">Figma</span>.<br />
+        <br />
+        My <span className="text-redishpinksh font-bold">Back-end</span> experience lies in <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">Node.js</span>, <span className="font-bold">NestJS</span>, <span className="font-bold">Vitest</span>, <span className="font-bold">Jest</span>, <span className="font-bold">PostgreeSQL</span>, <span className="font-bold">Docker</span>, <span className="font-bold">Prisma</span> and <span className="font-bold">Cloudflare R2</span>.
+        <br />
+        <br />
+        I'm constantly exploring new technologies.
       </p>
     </div>
   )
